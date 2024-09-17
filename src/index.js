4e88652c3bbf681711521bfc2f18523102a48597
ronit21102-js <ronit.roushan@kreedalabs.com>
2024-09-17 20:15:27 +0530

    utils

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+import app from "./app.js";
+const PORT = process.env.PORT || 8000;
+
+
+connectDB()
+.then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+})
+.catch((error) => {
+    console.log(error);
+});
+
+
+
+
